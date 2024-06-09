6e0cbe6ccd941549dfd72216eb4456b513dcd5d1
cuixiaoyiyi <cuibq@ios.ac.cn>
2024-06-09 14:17:47 +0800

    upload arifacts

---
+++ rpc.sh
@@ -0,0 +1,6 @@
+java -cp RPCBridge.jar cn.ac.ios.proxy.DPMain "ozone-1.4.0_060201" "./ozone-1.4.0" >ozone-1.4.0_060201.log 2>&1  
+java -cp RPCBridge.jar cn.ac.ios.proxy.DPMain "hadoop-3.4.0_060201" "./hadoop-3.4.0" >hadoop-3.4.0_060201.log 2>&1 
+java -cp RPCBridge.jar cn.ac.ios.proxy.DPMain "phoenix-hbase-2.5.0-5.2.0_060201" "./phoenix-hbase-2.5.0-5.2.0" >phoenix-hbase-2.5.0-5.2.0_060201.log 2>&1  
+java -cp RPCBridge.jar cn.ac.ios.proxy.DPMain "pravega-0.13.0_060201" "./pravega-0.13.0"  >pravega-0.13.0_060201.log 2>&1  
+java -cp RPCBridge.jar cn.ac.ios.proxy.DPMain "apache-tez-0.10.3_060201" "./apache-tez-0.10.3"  >apache-tez-0.10.3_060201.log 2>&1  
+java -cp RPCBridge.jar cn.ac.ios.proxy.DPMain "hbase-3.0.0-beta-1_060201" "./hbase-3.0.0-beta-1"  >hbase-3.0.0-beta-1_060201.log 2>&1   
